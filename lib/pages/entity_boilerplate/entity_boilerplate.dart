9869d7a81b8f897e8f79a3d5c20a6cfe413788e2
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-09-24 10:42:35 -0300

    [DRAFT] added textfield child scroll and test button

--- lib/pages/entity_boilerplate/entity_boilerplate.dart
+++ lib/pages/entity_boilerplate/entity_boilerplate.dart
@@ -1,15 +1,60 @@
+import 'package:boiler_plate_killer/pages/entity_boilerplate/entity_boilerplate_notifier.dart';
+import 'package:boiler_plate_killer/util/style/bpk_text_style.dart';
+import 'package:boiler_plate_killer/widgets/line_counter.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class EntityBoilerplate extends StatelessWidget {
-  const EntityBoilerplate({Key? key}) : super(key: key);
+class EntityBoilerplate extends StatefulWidget {
+  EntityBoilerplate({Key? key}) : super(key: key);
+
+  @override
+  State<EntityBoilerplate> createState() => _EntityBoilerplateState();
+}
+
+class _EntityBoilerplateState extends State<EntityBoilerplate> {
+  final TextEditingController _inputController = TextEditingController();
+
+  @override
+  void dispose() {
+    _inputController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(body: Column(
-        children: [
+    return Scaffold(
+        body: SingleChildScrollView(child: Consumer<EntityBoilerplateNotifier>(
+      builder: (context, notifier, _) {
+        return Column(children: [
           const Text('Entity Factor'),
-          const TextField(),
-        ],
-      ));
+          OutlinedButton(
+              onPressed: notifier.generateEntityFlag == true
+                  ? () {
+                      notifier.generateEntity(_inputController.text);
+                    }
+                  : null,
+              child: Text('Generate')),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                  fit: FlexFit.loose,
+                  child: LineCounter(lineLength: notifier.lineCount)),
+              Expanded(
+                  flex: 20,
+                  child: TextField(
+                      style: BpkTextStyle.editorText,
+                      controller: _inputController,
+                      keyboardType: TextInputType.multiline,
+                      onChanged: (String value) {
+                        notifier.checkStringLength(value);
+                      },
+                      maxLines: null))
+            ],
+          ),
+        ]);
+      },
+    )));
   }
 }
