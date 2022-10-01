96d766bedf4bb9f9d53480cab00aa58d9cb7a7cd
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-10-01 14:35:43 -0300

    [ADD] margin to place snackbar at the top of the screen

---
+++ lib/util/style/snackbar_edges.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/cupertino.dart';
+
+EdgeInsets snackBarInsets(BuildContext context) => EdgeInsets.only(
+    bottom: MediaQuery.of(context).size.height - 100, right: 20, left: 20);
