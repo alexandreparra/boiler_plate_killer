67a909daede05458088628d5f60c21b070c91c49
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-10-04 17:12:23 -0300

    [FIX] snackbar placement on screen

--- lib/util/style/snackbar_edges.dart
+++ lib/util/style/snackbar_edges.dart
@@ -1,4 +1,7 @@
+import 'package:boiler_plate_killer/util/extension/context_extensions.dart';
 import 'package:flutter/cupertino.dart';
 
-EdgeInsets snackBarInsets(BuildContext context) => EdgeInsets.only(
-    bottom: MediaQuery.of(context).size.height - 100, right: 20, left: 20);
+EdgeInsets snackBarInsets(BuildContext context) {
+  return EdgeInsets.only(
+      bottom: context.height() - 100, right: 20, left: context.width() * 0.5);
+}
