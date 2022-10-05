d92fcc24f14dff79fc7ae9ac43b331a58728ec30
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-10-05 10:38:40 -0300

    [REMOVE] snackbar edges and old error snackbar

--- lib/util/style/snackbar_edges.dart
+++
@@ -1,7 +0,0 @@
-import 'package:boiler_plate_killer/util/extension/context_extensions.dart';
-import 'package:flutter/cupertino.dart';
-
-EdgeInsets snackBarInsets(BuildContext context) {
-  return EdgeInsets.only(
-      bottom: context.height() - 100, right: 20, left: context.width() * 0.5);
-}
