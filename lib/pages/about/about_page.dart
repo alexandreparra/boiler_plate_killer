b8f375637187e7b01bd0bcb22f0b0c05654f2c9c
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-10-05 16:49:28 -0300

    [ADD] Scaffold

--- lib/pages/about/about_page.dart
+++ lib/pages/about/about_page.dart
@@ -1,18 +1,20 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-          Text('Boiler Plate Killer', style: TextStyle(fontSize: 18)),
-          SizedBox(height: 12),
-          Text('MIT LICENSE')
-        ],
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text('Boiler Plate Killer', style: TextStyle(fontSize: 22)),
+            SizedBox(height: 12),
+            Text('MIT LICENSE')
+          ],
+        ),
       ),
     );
   }
