c444d06db489e43987f5b7789ebaa6526306d14e
Alex <7588320-bluesden@users.noreply.gitlab.com>
2022-10-05 10:39:07 -0300

    [ADD] BpkSnackBar replacing old ErrorSnackBar

---
+++ lib/widgets/bpk_snack_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BpkSnackBar extends SnackBar {
+  const BpkSnackBar({
+    super.key,
+    required BuildContext context,
+    super.backgroundColor,
+    required super.content,
+    super.action,
+  });
+}
