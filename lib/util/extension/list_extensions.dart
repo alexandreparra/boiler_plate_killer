bc2e690d5940d3ea449fcea538f66587f6ccf800
Alexandre Parra <alexandre.medice1@gmail.com>
2023-07-28 11:31:22 -0300

    Removed unused files
    Most of them are now under the logic folder

--- lib/util/extension/list_extensions.dart
+++
@@ -1,3 +0,0 @@
-extension ListExt on List<String> {
-  List<String> takeFieldNames() => map((e) => e.split(' ')[1]).toList();
-}
--- lib/util/extension/string_extensions.dart
+++
@@ -1,9 +0,0 @@
-extension StringExt on String {
-  String getDataClassFileName(String classTermination) {
-    if (classTermination.isNotEmpty) {
-      return '${replaceAll(RegExp(r'(?<!^)(?=[A-Z])'), r'_').toLowerCase()}_${classTermination.toLowerCase()}.dart';
-    } else {
-      return '${replaceAll(RegExp(r'(?<!^)(?=[A-Z])'), r'_').toLowerCase()}.dart';
-    }
-  }
-}
